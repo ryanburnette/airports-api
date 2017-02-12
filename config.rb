741a1cc5d74d30a0a4843fdff6dc0b91a5876dd3
Ryan Burnette <ryan.burnette@gmail.com>
2017-02-12 16:19:30 -0500

    simplify usage of airports in config.rb

--- config.rb
+++ config.rb
@@ -1,6 +1,3 @@
-load "lib/airport.rb"
-AIRPORTS = Airport.get_airports
-
 set :layout, false
 page "*.json", :content_type => "application/json"
 
@@ -11,19 +8,16 @@ configure :build do
 end
 
 helpers do
-  def airports
-    AIRPORTS
-  end
 end
 
 proxy_file = "airport.json"
 ignore proxy_file
-AIRPORTS.drop(1).each do |a|
+load "lib/load_airports.rb"
+load_airports.each do |a|
   a.delete(:id)
   if a[:icao]
     proxy "/icao/#{a[:icao].to_s.downcase}.json", proxy_file, :locals => { :airport => a }
   end
-
   if a[:iata]
     proxy "/iata/#{a[:iata].to_s.downcase}.json", proxy_file, :locals => { :airport => a }
   end
